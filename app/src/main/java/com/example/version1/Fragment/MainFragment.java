284b1950923312c1be6e98edbaeb5ab7c01c36ee
Mi <1241869186@qq.com>
2019-12-11 17:31:13 +0800

    after adding some spinners and decorations

--- app/src/main/java/com/example/version1/Fragment/MainFragment.java
+++ app/src/main/java/com/example/version1/Fragment/MainFragment.java
@@ -8,13 +8,17 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckedTextView;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.version1.Activity.ResultActivity;
@@ -30,12 +34,26 @@ public class MainFragment extends Fragment {
     private EditText text;
     private Handler handler;
     private Spinner spinner;
+    private Spinner spinner2;
+    private  ArrayAdapter<String> adapter;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         View root = inflater.inflate(R.layout.fragment_main, container, false);
-        button = root.findViewById(R.id.button);
+       button = root.findViewById(R.id.button);
         text = root.findViewById(R.id.editText2);
         spinner = root.findViewById(R.id.spinner);
+        spinner2 = root.findViewById(R.id.spinner2);
+
+        adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item){
+
+        };
+        String level[] = getResources().getStringArray(R.array.array);//资源文件
+        for (int i = 0; i < level.length; i++) {
+            adapter.add(level[i]);
+        }
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
         return root;
     }
 
