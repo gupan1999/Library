75fd51925e48d0ad789e0c9d034f63f47ff7f361
gupan <1724845155@qq.com>
2019-12-15 21:19:34 +0800

    center and fix

--- app/src/main/java/com/example/version1/Activity/AcademicDissertations.java
+++ app/src/main/java/com/example/version1/Activity/AcademicDissertations.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.example.version1.Model.Academic;
 import com.example.version1.Model.AcademicDissertation;
 import com.example.version1.R;
 import com.example.version1.Util.BaseRecyclerAdapter;
@@ -19,19 +20,27 @@ import java.util.List;
 public class AcademicDissertations extends AppCompatActivity {
     private RecyclerView academicDissertationre;
     private BaseRecyclerAdapter baseRecyclerAdapter;
-    private List<AcademicDissertation> academicDissertationList=new ArrayList<AcademicDissertation>();
+    //private List<AcademicDissertation> academicDissertationList=new ArrayList<AcademicDissertation>();
+    private List<Academic> academicList=new ArrayList<Academic>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_academic_dissertation);
         academicDissertationre=findViewById(R.id.academicdissertationre);
-        AcademicDissertation academicDissertation=new AcademicDissertation("散文江湖","形散神不散，优美娓娓来。散文，以其优美的文字，丰富的意向，舒缓的情调让人喜爱。或歌颂，或致意，或赞美...",R.mipmap.book2);
-        AcademicDissertation academicDissertation1=new AcademicDissertation("时间移民","选取25岁以下的人类成员向未来移民。旅行队伍进行了多次停留，但每一次的地球环境都不再适合人类居住...",R.mipmap.book1);
-        academicDissertationList.add(academicDissertation);
-        academicDissertationList.add(academicDissertation1);
+        //AcademicDissertation academicDissertation=new AcademicDissertation("散文江湖","形散神不散，优美娓娓来。散文，以其优美的文字，丰富的意向，舒缓的情调让人喜爱。或歌颂，或致意，或赞美...",R.mipmap.book2);
+        //AcademicDissertation academicDissertation1=new AcademicDissertation("时间移民","选取25岁以下的人类成员向未来移民。旅行队伍进行了多次停留，但每一次的地球环境都不再适合人类居住...",R.mipmap.book1);
+        Academic academic=new Academic("基于虚拟演播室的微课设计与开发——以《社会心理学》为例");
+        Academic academic1=new Academic("基于人工智能(AI)技术的光通信网络:Hope还是Hype");
+        Academic academic2=new Academic("边缘计算在采煤机控制系统中的应用");
 
-        baseRecyclerAdapter=new BaseRecyclerAdapter<AcademicDissertation>(this,R.layout.academicdissertationitem,academicDissertationList){
+        //academicDissertationList.add(academic);
+       // academicDissertationList.add(academic1);
+        academicList.add(academic);
+        academicList.add(academic1);
+        academicList.add(academic2);
+        /*
+        baseRecyclerAdapter=new BaseRecyclerAdapter<AcademicDissertation>(this,R.layout.academic_item,academicDissertationList){
             @Override
             public void convert(BaseViewHolder holder, AcademicDissertation academicDissertation) {
                 holder.setText(R.id.textView5,academicDissertation.getBookname());
@@ -44,7 +53,18 @@ public class AcademicDissertations extends AppCompatActivity {
 
             }
         };
+        */
+        baseRecyclerAdapter=new BaseRecyclerAdapter<Academic>(this,R.layout.academic_item,academicList){
+            @Override
+            public void convert(BaseViewHolder holder, Academic academic) {
+               holder.setText(R.id.academictitle,academic.getTitle());
+            }
 
+            @Override
+            public void setting(BaseViewHolder holder) {
+
+            }
+        };
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         academicDissertationre.setLayoutManager(layoutManager);
         academicDissertationre.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL)); //item间的分割线
---
+++ app/src/main/java/com/example/version1/Model/Academic.java
@@ -0,0 +1,20 @@
+package com.example.version1.Model;
+
+public class Academic {
+    private String title;
+
+    public Academic() {
+    }
+
+    public Academic(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
